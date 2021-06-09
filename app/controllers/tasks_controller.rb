0c4706eb45cd4d134f675747eeeae16598e0cbea
sho.sasagawa <sasasho79@gmail.com>
2021-06-09 19:57:08 +0000

    user_id

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find_by(id: session[:user_id])
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -28,11 +28,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find_by(id: session[:user_id]) 
+    @task = Task.find(params[:id])  
   end
 
   def update
-    @task = Task.find_by(id: session[:user_id])
+    @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find_by(id: session[:user_id])
+    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
